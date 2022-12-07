2e839daafa80c03a4e1a0cab9fd43f4a56e479e1
Lucas de Sousa <Lucasads18@outlook.com>
2022-12-07 10:20:54 -0300

    Adicionar arquivos de projeto.

---
+++ MySql.cs
@@ -0,0 +1,109 @@
+﻿using MySqlConnector;
+using System.Collections;
+using System.Data;
+
+namespace PublicUtility.Sql.MySql
+{
+    public class DB : IDisposable
+    {
+        private readonly MySqlConnection con = null;
+        private MySqlTransaction tran = null;
+        private MySqlCommand cmd = null;
+
+        #region PRIVATE METHODS
+
+        private void RollBack()
+        {
+            if (cmd.Transaction != null)
+                cmd.Transaction.Rollback();
+        }
+
+        private void Commit()
+        {
+            if (cmd.Transaction != null)
+                cmd.Transaction.Commit();
+        }
+
+        private MySqlConnection Open()
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+                tran = con.BeginTransaction();
+            }
+            return con;
+        }
+
+        private void Close()
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+        }
+
+        #endregion
+
+        private DB(MySqlConnectionStringBuilder builder)
+        {
+            con = new MySqlConnection(builder.ConnectionString);
+        }
+
+
+        public static DB GetConn(MySqlConnectionStringBuilder connectionBuilder) => new(connectionBuilder);
+
+        public string ExecCmd(MySqlCommand command)
+        {
+            cmd = command;
+            try
+            {
+                cmd.Connection = Open();
+                cmd.Transaction = tran;
+                cmd.ExecuteNonQuery();
+                Commit();
+                return string.Format($" [OK] - {DateTime.UtcNow}");
+            }
+            catch (Exception ex)
+            {
+                RollBack();
+
+                return string.Format($" [ERRO] - {DateTime.UtcNow} # {ex.Message} ");
+            }
+        }
+
+        public string ExecQuery(string query) => ExecCmd(new MySqlCommand(query));
+
+        public DataTable ReturnData(MySqlCommand command)
+        {
+            var table = new DataTable();
+            cmd = command;
+            try
+            {
+                var adapter = new MySqlDataAdapter();
+                cmd.Connection = Open();
+                cmd.Transaction = tran;
+
+                adapter.SelectCommand = cmd;
+                adapter.Fill(table);
+                Commit();
+            }
+            catch
+            {
+                table = null;
+                RollBack();
+            }
+            return table;
+        }
+
+        public DataTable ReturnData(string query) => ReturnData(new MySqlCommand(query));
+
+        public T ReturnData<T>(string query) where T : IEnumerable => ReturnData(query).DeserializeTable<T>();
+
+        public T ReturnData<T>(MySqlCommand command) where T : IEnumerable => ReturnData(command).DeserializeTable<T>();
+
+        public void Dispose()
+        {
+            Close();
+            GC.SuppressFinalize(this);
+            GC.Collect();
+        }
+    }
+}
---
+++ SqlServer.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Collections;
+using System.Data;
+
+namespace PublicUtility.Sql.SqlServer {
+  public class DB: IDisposable {
+    private readonly SqlConnection con = null;
+    private SqlTransaction tran = null;
+    private SqlCommand cmd = null;
+
+    #region PRIVATE METHODS
+
+    private void RollBack() {
+      if(cmd.Transaction != null)
+        cmd.Transaction.Rollback();
+    }
+
+    private void Commit() {
+      if(cmd.Transaction != null)
+        cmd.Transaction.Commit();
+    }
+
+    private SqlConnection Open() {
+      if(con.State == ConnectionState.Closed) {
+        con.Open();
+        tran = con.BeginTransaction();
+      }
+      return con;
+    }
+
+    private void Close() {
+      if(con.State == ConnectionState.Open)
+        con.Close();
+    }
+
+    #endregion
+
+    private DB(string connectionString) {
+      con = new SqlConnection(connectionString);
+    }
+
+    public static DB GetConn(string connectionString) => new(connectionString);
+
+    public string ExecCmd(SqlCommand command) {
+      cmd = command;
+      try {
+        cmd.Connection = Open();
+        cmd.Transaction = tran;
+        cmd.ExecuteNonQuery();
+        Commit();
+        return string.Format($" [OK] - {DateTime.UtcNow}");
+      } catch(Exception ex) {
+        RollBack();
+
+        return string.Format($" [ERRO] - {DateTime.UtcNow} # {ex.Message} ");
+      }
+    }
+
+    public string ExecQuery(string query) => ExecCmd(new SqlCommand(query));
+
+    public DataTable ReturnData(SqlCommand command) {
+      var table = new DataTable();
+      cmd = command;
+      try {
+        var adapter = new SqlDataAdapter();
+        cmd.Connection = Open();
+        cmd.Transaction = tran;
+
+        adapter.SelectCommand = cmd;
+        adapter.Fill(table);
+        Commit();
+      } catch {
+        table = null;
+        RollBack();
+      }
+      return table;
+    }
+
+    public DataTable ReturnData(string query) => ReturnData(new SqlCommand(query));
+
+    public T ReturnData<T>(string query) where T : IEnumerable => ReturnData(query).DeserializeTable<T>();
+
+    public T ReturnData<T>(SqlCommand command) where T : IEnumerable => ReturnData(command).DeserializeTable<T>();
+
+    public void Dispose() {
+      Close();
+      GC.SuppressFinalize(this);
+      GC.Collect();
+    }
+  }
+}
