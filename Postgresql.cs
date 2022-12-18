bc08ee5d50a99d2197324b4ba0a241b24014415e
Lucas de Sousa <Lucasads18@outlook.com>
2022-12-18 09:50:00 -0300

    include a postgresql

---
+++ Postgresql.cs
@@ -0,0 +1,92 @@
+﻿using Npgsql;
+using PublicUtility.Extension;
+using System.Collections;
+using System.Data;
+
+namespace PublicUtility.Sql.Postgresql {
+  public class DB: IDisposable {
+    private readonly NpgsqlConnection con = null;
+    private NpgsqlTransaction tran = null;
+    private NpgsqlCommand cmd = null;
+
+    #region PRIVATE METHODS
+
+    private void RollBack() {
+      if(cmd.Transaction != null)
+        cmd.Transaction.Rollback();
+    }
+
+    private void Commit() {
+      if(cmd.Transaction != null)
+        cmd.Transaction.Commit();
+    }
+
+    private NpgsqlConnection Open() {
+      if(con.State == ConnectionState.Closed) {
+        con.Open();
+        tran = con.BeginTransaction();
+      }
+      return con;
+    }
+
+    private void Close() {
+      if(con.State == ConnectionState.Open)
+        con.Close();
+    }
+
+    #endregion
+
+    private DB(string connectionString) {
+      con = new NpgsqlConnection(connectionString);
+    }
+
+    public static DB GetConn(string connectionString) => new(connectionString);
+
+    public string ExecCmd(NpgsqlCommand command) {
+      cmd = command;
+      try {
+        cmd.Connection = Open();
+        cmd.Transaction = tran;
+        cmd.ExecuteNonQuery();
+        Commit();
+        return string.Format($" [OK] - {DateTime.UtcNow}");
+      } catch(Exception ex) {
+        RollBack();
+
+        return string.Format($" [ERRO] - {DateTime.UtcNow} # {ex.Message} ");
+      }
+    }
+
+    public string ExecQuery(string query) => ExecCmd(new NpgsqlCommand(query));
+
+    public DataTable ReturnData(NpgsqlCommand command) {
+      var table = new DataTable();
+      cmd = command;
+      try {
+        var adapter = new NpgsqlDataAdapter();
+        cmd.Connection = Open();
+        cmd.Transaction = tran;
+
+        adapter.SelectCommand = cmd;
+        adapter.Fill(table);
+        Commit();
+      } catch {
+        table = null;
+        RollBack();
+      }
+      return table;
+    }
+
+    public DataTable ReturnData(string query) => ReturnData(new NpgsqlCommand(query));
+
+    public T ReturnData<T>(string query) where T : IEnumerable => ReturnData(query).DeserializeTable<T>();
+
+    public T ReturnData<T>(NpgsqlCommand command) where T : IEnumerable => ReturnData(command).DeserializeTable<T>();
+
+    public void Dispose() {
+      Close();
+      GC.SuppressFinalize(this);
+      GC.Collect();
+    }
+  }
+}
